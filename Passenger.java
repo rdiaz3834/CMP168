ea5a66d063e40c7f24d0aff5cbf4012000da1d2e
rdiaz3834 <33561904+rdiaz3834@users.noreply.github.com>
2022-03-04 07:28:50 -0500

    Add files via upload

---
+++ Passenger.java
@@ -0,0 +1,176 @@
+
+import java.util.Scanner;
+
+public class Passenger {
+	//-----------------------------Start Main--------------------------------
+	
+	
+	
+	//----------------------------End Main----------------------------------
+	Scanner scnr = new Scanner(System.in);
+
+	private String name;
+	private int birthYear;
+	private double weight;
+	private char gender;
+	private int numCarryOn;
+	
+	//------------------------Object----------------------------
+	public Passenger() {
+		this.name = "";
+		this.birthYear = 1900;
+		this.weight = 0.0;
+		this.gender = 'u';
+		this.numCarryOn = 0;
+		
+		
+	}
+	
+	//-------------------------Constructor-----------------------
+	 public Passenger(String name, int birthYear, double weight, char gender, int numCarryOn) {
+	        this.name = name;
+	        this.birthYear = birthYear;
+
+	        if (weight < 0) {
+	            this.weight = -1;
+	        } else {
+	            this.weight = weight;
+	        }
+
+	        if (gender == 'm' || gender == 'f') {
+	            this.gender = gender;
+	        } else {
+	            this.gender = 'u';
+	        }
+
+	        if (numCarryOn < 0) {
+	            this.numCarryOn = 0;
+	        } else if (numCarryOn > 2) {
+	            this.numCarryOn = 2;
+	        } else {
+	            this.numCarryOn = numCarryOn;
+	        }
+	    }
+
+	    // method to calculate age
+	    public int calculateAge(int currentYear) {
+	        if (currentYear < birthYear) {
+	            return -1;
+	        } else {
+	            return currentYear - birthYear;
+	        }
+	    }
+
+	    // method gain weight
+	    public void gainWeight() {
+	        weight = weight + 1;
+	    }
+
+	    // method to gain weight by amount
+	    public void gainWeight(double amount) {
+	        if (weight + amount < 0) {
+	            System.out.println("Weight cannot drop below 0");
+	        } else {
+	            weight = weight + amount;
+	        }
+	    }
+
+	    // start of getter and setter
+	    public String getName() {
+	        return name;
+	    }
+
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+
+	    public int getBirthYear() {
+	        return birthYear;
+	    }
+
+	    public void setBirthYear(int birthYear) {
+	        this.birthYear = birthYear;
+	    }
+
+	    public double getWeight() {
+	        return weight;
+	    }
+
+	    public void setWeight(double weight) {
+	        if (weight < 0) {
+	            this.weight = -1;
+	        } else {
+	            this.weight = weight;
+	        }
+	    }
+
+	    public char getGender() {
+	        return gender;
+	    }
+
+	    public void setGender(char gender) {
+	        if (gender == 'm' || gender == 'f') {
+	            this.gender = gender;
+	        } else {
+	            this.gender = 'u';
+	        }
+	        
+	    }
+
+	    public int getNumCarryOn() {
+	        return numCarryOn;
+	    }
+
+	    public void setNumCarryOn(int numCarryOn) {
+	        if (numCarryOn < 0) {
+	            this.numCarryOn = 0;
+	        } else if (numCarryOn > 2) {
+	            this.numCarryOn = 2;
+	        } else {
+	            this.numCarryOn = numCarryOn;
+	        }
+	    }
+	    // end of getter and setter
+
+	    // method to check is female
+	    public boolean isFemale() {
+	        if (gender == 'f') {
+	            return true;
+	        } else {
+	            return false;
+	        }
+	    }
+
+	    // method to check is male
+	    public boolean isMale() {
+	        if (gender == 'm') {
+	            return true;
+	        } else {
+	            return false;
+	        }
+	    }
+
+	    // method to loose weight
+	    public void loseWeight() {
+	        if (weight <= 0) {
+	            System.out.println("Weight cannot drop below 0");
+	        } else {
+	            weight = weight - 1;
+	        }
+	    }
+
+	    // method to loose weight by amount
+	    public void loseWeight(double amount) {
+	        if (weight - amount < 0) {
+	            System.out.println("Weight cannot drop below 0");
+	        } else {
+	            weight = weight - amount;
+	        }
+	    }
+
+	    // method print details
+	    public void printDetails() {
+	        System.out.printf("Name: %20s | Year of Birth: %4d | Weight: %10.2f | Gender: %c | NumCarryOn: %2d\n"
+	                , name, birthYear, weight, gender, numCarryOn);
+	    }
+}
